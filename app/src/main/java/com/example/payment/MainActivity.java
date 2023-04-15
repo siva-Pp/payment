2c1bd74e5ea1d786d7374debc960cd93d3c21190
siva-Pp <130553261+siva-Pp@users.noreply.github.com>
2023-04-15 16:43:03 +0530

    check network

--- app/src/main/java/com/example/payment/MainActivity.java
+++ app/src/main/java/com/example/payment/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText amount;
     Button pay;
+    TextView retry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         amount = findViewById(R.id.amount);
         pay = findViewById(R.id.pay);
+        retry = findViewById(R.id.retry);
 
         if (!checkInternet()){
 
@@ -32,8 +38,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean checkInternet(){
+
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        return  connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+
+        if (connectivityManager != null){
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+
+                NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork());
+
+                if (networkCapabilities != null){
+
+                    if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)){
+
+                        return true;
+
+                    } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+
+                        return true;
+
+                    } else{
+
+                        return networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET);
+                        
+                    }
+
+                }
+
+            }else {
+
+                ConnectivityManager ca = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = ca.getActiveNetworkInfo();
+                return networkInfo != null && networkInfo.isConnectedOrConnecting();
+
+            }
+
+        }
+//        return  connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+        return false;
+
     }
 
 }
